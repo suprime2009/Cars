8d2cf7f30e8410dd30ccf2ba95652ab1226ceba9
suprime2009 <apolopavel@gmail.com>
2015-08-29 20:01:59 +0300

    changes in Tractor

--- src/main/java/com/softserve/model/Tractor.java
+++ src/main/java/com/softserve/model/Tractor.java
@@ -12,6 +12,6 @@ public class Tractor extends Car {
     public Tractor(String model, double engine, double benzinPer100Km, int averSpeed, 
 	    int id, String name) {
 	super(model, engine, benzinPer100Km, averSpeed);
-	this.cargo = new Cargo (id, name);
+	this.cargo = new Cargo(id, name);
     }
 }

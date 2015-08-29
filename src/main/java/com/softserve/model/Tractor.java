d204e3873330936c4aa12f34cc6bc0993b3cbda1
suprime2009 <apolopavel@gmail.com>
2015-08-29 19:55:17 +0300

    some changes

--- src/main/java/com/softserve/model/Tractor.java
+++ src/main/java/com/softserve/model/Tractor.java
@@ -2,22 +2,16 @@ package com.softserve.model;
 
 /**
  * Tractor class
+ * 
  * @author UserMaryana
  *
  */
 public class Tractor extends Car {
-	public Cargo cargo;
+    public Cargo cargo;
 
-	public Tractor(String model, double engine, double benzinPer100Km, int averSpeed) {
-		super(model, engine, benzinPer100Km, averSpeed);
-	}
-
-	/*
-	 * method shows a number of fuel for needed kilemetres
-	 */
-	public double needOfFuel (double klm){
-		return klm*10;
-	} 
-
-	
+    public Tractor(String model, double engine, double benzinPer100Km, int averSpeed, 
+	    int id, String name) {
+	super(model, engine, benzinPer100Km, averSpeed);
+	this.cargo = new Cargo (id, name);
+    }
 }
